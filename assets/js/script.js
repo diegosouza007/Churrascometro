d2dfa4ef30d1be2b5e854a1e975e6fa32471f74a
diegosouza007 <dsouza@yahoo.com>
2021-07-06 23:38:36 -0300

    adding new files e first contents

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/** Regras de negócio
+
+Carne: 400gr por pessoa. Acima de 6h: 650gr por pessoa
+Cerveja: 1.200ml por Pessoa. Acima de 6h: 2000ml por pessoa
+Refrigerante/Água: 1.000ml por pessoa. Acima de 6h: 1.500ml por pessoa
+
+Crianças valem por 0,5 (metade) **/
